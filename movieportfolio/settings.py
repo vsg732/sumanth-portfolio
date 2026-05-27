65aabf3bb2f007020f61246918132b47e48ce72d
vsg732 <gunturuvidyasagar7@gmail.com>
2026-05-27 14:23:16 +0530

    Update settings.py

--- movieportfolio/settings.py
+++ movieportfolio/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
@@ -123,6 +123,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+
+
 
 import os
 
