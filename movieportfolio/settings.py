80351e722e175ab19d2ee5f6cac7595e98cfca70
vsg732 <gunturuvidyasagar7@gmail.com>
2026-05-27 15:02:07 +0530

    Update settings.py

--- movieportfolio/settings.py
+++ movieportfolio/settings.py
@@ -116,12 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
